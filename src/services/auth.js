f633703286ebc9af71bcff6bbd98018512b8845b
Viktor1107 <162905444+Viktor1107@users.noreply.github.com>
2025-07-29 17:34:37 +0200

    add router login

--- src/services/auth.js
+++ src/services/auth.js
@@ -12,3 +12,16 @@ export const registerUser = async (payload) => {
     password: encryptedPassword,
   });
 };
+
+export const loginUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+
+  if (!user) {
+    throw createHttpError(401, 'User not found');
+  }
+  const isEqual = await bcrypt.compare(payload.password, user.password);
+
+  if (!isEqual) {
+    throw createHttpError(401, 'Unauthorized');
+  }
+};
